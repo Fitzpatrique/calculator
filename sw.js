e010a13bf91d9c3930ecabecdbb8b59299f508ea
Ogbonna Patrick <53985539+Fitzpatrique@users.noreply.github.com>
2023-04-03 20:19:28 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAll(["./","./index.css","./images/eval.png","./index.js"]);
+            return cache.addAll(["./calculator","./index.css","./images/eval.png"]);
         })
     );
 });
